5a93d89fce7bbae12f8a87e82699eb54b5e39830
pbs0325 <6542246@gmail.com>
2024-02-26 09:06:15 +0900

    aa

--- jswork/jsjquerywork/js/popup.js
+++ jswork/jsjquerywork/js/popup.js
@@ -1,12 +1,13 @@
 function pop1()
 {
     window.open('../image/쇼핑몰사진/1.jpg','event','width=500,height=500');
+
+   
 }
 
 function pop2()
 {
-    newpopup=window.open('../image/logoimg/box.png','','width=400,height=500')
+     newpopup=window.open('../image/logoImg/box.png','','width=400,height=500');
 
-    setTimeout('newpopup.close()',3000);
+   setTimeout('newpopup.close()',3000);
 }
-
